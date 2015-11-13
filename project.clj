527d07df34126607cddd6c4de0acc3d3085a5ab5
Hidenari Nozaki <hidenari@Hidenaris-MBP.fritz.box>
2015-11-13 18:00:08 +1300

    Add deployment to heroku

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]]
+
+  :min-lein-version "2.0.0"
+
+  :uberjar-name "webdev.jar"
+
+  :main webdev.core
+
+  :profiles {:dev
+             {:main webdev.core/-dev-main}})
--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,6 +1,26 @@
-(ns webdev.core)
+(ns webdev.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn greet [req]
+  {:status 200
+   :body "Hello, World!"
+   :headers {}})
+
+(defn goodbye [req]
+  {:status 200
+   :body "Goodbye, Cruel World!"
+   :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (GET "/goodbye" [] goodbye)
+  (not-found "Page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
