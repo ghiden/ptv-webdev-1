a50dfd2875b23dc07f205da41e110ad50f6db935
Hidenari <hidenari@gmail.com>
2015-11-13 21:33:44 +1300

    Add jdbc and postgresql dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.4.0"]
-                 [compojure "1.4.0"]]
+                 [compojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
 
   :min-lein-version "2.0.0"
 
